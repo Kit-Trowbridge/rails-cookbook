a45cdd2ceb706de5e8039c21a5deb7f5eeb84a51
Kit Trowbridge <kittrow.dev@gmail.com>
2025-01-13 12:12:10 +0000

    added form to bookmarks new view and coded new and create actions

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,21 @@
 class BookmarksController < ApplicationController
+  def new
+    @category = Category.find(params[:category_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @category = Category.find(params[:category_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.category = @category
+    if @bookmark.save
+      redirect_to category_path(@category)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:name)
+  end
 end
