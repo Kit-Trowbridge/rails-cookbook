38f79cc21e713623b502eb089676d053efff60e7
Kit Trowbridge <kittrow.dev@gmail.com>
2025-01-13 11:39:14 +0000

    added simpleform, coded new and create actions for lists

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,4 +10,16 @@ class CategoriesController < ApplicationController
   def new
     @category = Category.new
   end
+
+  def create
+    @category = Category.new(category_params)
+    @category.save
+    redirect_to category_path(@category)
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
