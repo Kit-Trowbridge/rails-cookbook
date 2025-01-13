afc5ec1f7e2c2505a43b7e54f6652ba5e8ef1226
Kit Trowbridge <kittrow.dev@gmail.com>
2025-01-13 12:59:05 +0000

    added bookmark destroy method after some trial and error

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -15,6 +15,14 @@ class BookmarksController < ApplicationController
     end
   end
 
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to category_path(@bookmark.category), status: :see_other
+  end
+
+  private
+
   def bookmark_params
     params.require(:bookmark).permit(:comment, :category_id, :recipe_id)
   end
