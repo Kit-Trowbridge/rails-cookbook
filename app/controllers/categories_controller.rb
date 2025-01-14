b97f32a82eeb558976b3f41c172036b6fb04fad9
Kit Trowbridge <kittrow.dev@gmail.com>
2025-01-14 12:09:11 +0000

    started to add styling of cards with category image

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -34,6 +34,6 @@ class CategoriesController < ApplicationController
   end
 
   def category_params
-    params.require(:category).permit(:name)
+    params.require(:category).permit(:name, :image_url)
   end
 end
